472b6a6b026dc7fca6a8c305650da73ac7c900fb
4bysss <4byzz@zee.com>
2020-11-12 19:16:07 +0100

    Aye Added th4t c0wd.
    And this is a long description.
    It could take as many lines as needed.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+ * b3tt3rth4nl4m0h!
+ */
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	cout << "th33z 33z 337!" << endl;
+	vector<string> words = {
+		"Unglab",
+		"Glieben",
+		"Glassen",
+		"Globen"
+	};
+	for (auto str : words) {
+		cout << str << "\t";
+	}
+	cout << endl;
+	//Daeeh daeeh daeeh mah proc!
+	return 0;
+}
